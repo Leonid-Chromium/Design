66f95e28586f6a1530e3b5405a113db4c1b7075e
Leonid-Chromium <l.khromenko@gmail.com>
2021-11-30 16:38:11 +0700

    SecondLesson

--- Design/Design/UCs/VisitorUC.xaml.cs
+++ Design/Design/UCs/VisitorUC.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Design
+namespace Design.UCs
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для VisitorUC.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VisitorUC : UserControl
     {
-        public MainWindow()
+        public VisitorUC()
         {
             InitializeComponent();
         }
---
+++ Design/Design/Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Design.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            actualUC = mainUC;
+        }
+
+        private void mainUC_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (mainUC.Visibility == Visibility.Visible)
+                Header.Visibility = Visibility.Collapsed;
+            else
+                Header.Visibility = Visibility.Visible;
+        }
+
+        public static UserControl actualUC;
+
+        public static void ChangeUC(UserControl newUC)
+        {
+            if (newUC == actualUC)
+                newUC.Visibility = Visibility.Collapsed;
+            else
+            {
+                actualUC.Visibility = Visibility.Collapsed;
+                newUC.Visibility = Visibility.Visible;
+            } 
+        }
+    }
+}
