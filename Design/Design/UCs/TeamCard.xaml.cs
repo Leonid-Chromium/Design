0c3b40315e0e7c3829bd0ca8332cda9b6c14b1bd
Leonid-Chromium <l.khromenko@gmail.com>
2022-02-24 12:00:44 +0700

    Save

---
+++ Design/Design/UCs/TeamCard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design.UCs
+{
+    /// <summary>
+    /// Логика взаимодействия для TeamCard.xaml
+    /// </summary>
+    public partial class TeamCard : UserControl
+    {
+        public string TeamNameStr { get; set; }
+        public string RosterStr { get; set; }
+        public string MatchupStr { get; set; }
+        public string FirstLineupStr { get; set; }
+
+    public TeamCard()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
